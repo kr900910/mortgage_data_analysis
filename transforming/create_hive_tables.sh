474f712f0e53a106055edcf87480231a40fc97ae
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-03 22:52:43 -0500

    Updated instructions and added draft hive shell script

---
+++ transforming/create_hive_tables.sh
@@ -0,0 +1,18 @@
+# shell script to run hive queries to create base and hive tables and transform them
+
+# Switch to folder with base hive ddl script
+cd ../loading_and_modeling
+
+# This creates Hive metadata for fannie_mae_acq, fannie_mae_perf, freddie_mac_acq, and freddie_mac_perf
+hive -f hive_base_ddl.sql
+
+# Switch back to transformations folder for manipulating the base hdfs data
+cd ../transforming
+
+# This create Hive metadata for loan_acq_data which combines fannie_mae_acq and freddie_mae_acq
+hive -f loan_acq_data.sql
+
+# This create Hive metadata for loan_perf_data which combines fannie_mae_perf and freddie_mae_perf
+hive -f loan_perf_data.sql
+
+
