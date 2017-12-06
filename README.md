9198cd71b7c5abbff54061229bb28096a28b32c0
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-06 13:53:59 -0500

    Minor readme edits and formatting of SQL script for readability

--- README.md
+++ README.md
@@ -6,13 +6,13 @@
 3. Pull mortgage-data-analysis repository in EC2 instance (`git clone https://github.com/kr900910/mortgage-data-analysis.git`).
 4. Create temp_download directory inside mortgage-data-analysis (`mkdir temp_download`).
 
-# Download the data into the downloading folder
+# Download the data
 5. Go to mortgage-data-analysis/loading_and_modeling, and `pip install requests==2.5.3`.
 6. Type `python download_freddie_mac.py`. Enter credentials and quarters to download when prompted. This downloads zip files into the current folder for each quarter.
 7. Type `python download_fannie_mae.py`. Enter credentials and quarters to download when prompted. This downloads zip files into the current folder for each quarter.
 
 # Move the data into HDFS directory
-8. Start Hadoop, postgres, Hive in EC2 instance. Type `su - w205`. This changes the user.
+8. Start Hadoop, postgres, and Hive in EC2 instance.
 10. If this is your first time, type `. create_hdfs_dir.sh`. This creates necessary HDFS folders.
 11. Type `. unzip_to_HDFS.sh`. This unzips the zipped files into mortgage-data-analysis/temp_download, removes the zipped files, loads unzipped files to HDFS, and removes the unzipped files.
 
--- transforming/loan_perf_data.sql
+++ transforming/loan_perf_data.sql
@@ -1,16 +1,38 @@
 DROP TABLE loan_perf_data;
 CREATE TABLE loan_perf_data AS
-SELECT LOAN_ID, to_date(MONTHLY_RPT_PRD) as MONTHLY_RPT_PRD, LAST_RT,
-LAST_UPB, LOAN_AGE, MONS_TO_LEGAL_MAT,
-to_date(MATURITY_DATE) as MATURITY_DATE, MSA, DELQ_STATUS, MOD_FLAG,
-ZERO_BAL_CODE, to_date(ZB_DTE) as ZB_DTE, to_date(LPI_DTE) as LPI_DTE,
-NS_PROCS, "FANNIE_MAE" as AGENCY
+SELECT LOAN_ID, 
+	to_date(MONTHLY_RPT_PRD) as MONTHLY_RPT_PRD, 
+	LAST_RT,
+	LAST_UPB, 
+	LOAN_AGE, 
+	MONS_TO_LEGAL_MAT,
+	to_date(MATURITY_DATE) as MATURITY_DATE, 
+	MSA, 
+	DELQ_STATUS, 
+	MOD_FLAG,
+	ZERO_BAL_CODE, 
+	to_date(ZB_DTE) as ZB_DTE, 
+	to_date(LPI_DTE) as LPI_DTE,
+	NS_PROCS, 
+	"FANNIE_MAE" as AGENCY
 from fannie_mae_perf
 UNION ALL
-SELECT fred_p.ID_loan as LOAN_ID, to_date(Period) as MONTHLY_RPT_PRD, New_Int_rt as LAST_RT,
-Act_endg_upb as LAST_UPB, loan_age as LOAN_AGE, mths_remng as MONS_TO_LEGAL_MAT,
-to_date(dt_matr) as MATURITY_DATE, cd_msa as MSA, delq_sts as DELQ_STATUS, flag_mod as MOD_FLAG,
-CD_Zero_BAL as ZERO_BAL_CODE, to_date(Dt_zero_BAL) as ZB_DTE, to_date(Dt_Lst_Pi) as LPI_DTE,
-Net_Sale_Proceeds as NS_PROCS, "FREDDIE_MAC" as AGENCY
+SELECT 
+	fred_p.ID_loan as LOAN_ID, 
+	to_date(Period) as MONTHLY_RPT_PRD, 
+	New_Int_rt as LAST_RT,
+	Act_endg_upb as LAST_UPB, 
+	loan_age as LOAN_AGE, 
+	mths_remng as MONS_TO_LEGAL_MAT,
+	to_date(dt_matr) as MATURITY_DATE, 
+	cd_msa as MSA, 
+	delq_sts as DELQ_STATUS, 
+	flag_mod as MOD_FLAG,
+	CD_Zero_BAL as ZERO_BAL_CODE, 
+	to_date(Dt_zero_BAL) as ZB_DTE, 
+	to_date(Dt_Lst_Pi) as LPI_DTE,
+	Net_Sale_Proceeds as NS_PROCS, 
+	"FREDDIE_MAC" as AGENCY
 from freddie_mac_perf fred_p
-left join freddie_mac_acq fred_a on fred_p.ID_loan = fred_a.id_loan
+left join freddie_mac_acq fred_a 
+	on fred_p.ID_loan = fred_a.id_loan
