3a1f7ad9d7e2bbfd340a703af3a435e801e552c4
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-06 18:37:49 -0500

    Removed duplicate column

--- transforming/acq_perf_data.sql
+++ transforming/acq_perf_data.sql
@@ -1,4 +1,18 @@
 DROP TABLE acq_perf_data;
 CREATE TABLE acq_perf_data AS
-SELECT acq.*, perf.* from loan_acq_data acq
-JOIN loan_perf_data perf on acq.LOAN_ID = perf.LOAN_ID
+SELECT acq.*, 
+	perf.MONTHLY_RPT_PRD,
+	perf.LAST_RT,
+	perf.LAST_UPB,
+	perf.LOAN_AGE, 
+	perf.MONS_TO_LEGAL_MAT,
+	perf.DELQ_STATUS, 
+	perf.MOD_FLAG,
+	perf.ZERO_BAL_CODE, 
+	perf.ZB_DTE, 
+	perf.LPI_DTE,
+	perf.NS_PROCS, 
+	perf.AGENCY as PERF_AGENCY
+from loan_acq_data acq
+JOIN loan_perf_data perf 
+	on acq.LOAN_ID = perf.LOAN_ID
