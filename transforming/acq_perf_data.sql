7a8972586989bae47db47e6432e90a7746cbce14
kr900910 <kr900910@hotmail.com>
2017-12-06 08:13:22 +0000

    acq_perf_data.sql

--- transforming/acq_perf_data.sql
+++ transforming/acq_perf_data.sql
@@ -1,4 +1,4 @@
 DROP TABLE acq_perf_data;
 CREATE TABLE acq_perf_data AS
-SELECT * from loan_perf_data perf
-JOIN loan_acq_data acq on perf.LOAN_ID = acq.LOAN_ID
+SELECT acq.*, perf.* from loan_acq_data acq
+JOIN loan_perf_data perf on acq.LOAN_ID = perf.LOAN_ID
