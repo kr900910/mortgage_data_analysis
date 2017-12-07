05698b527168844e18f63b3ccbbcef66ceba6a3b
Seung Hun Ham <kr900910@hotmail.com>
2017-12-06 22:45:26 -0800

    Data type performance data

--- transforming/loan_perf_data.sql
+++ transforming/loan_perf_data.sql
@@ -2,31 +2,31 @@ DROP TABLE loan_perf_data;
 CREATE TABLE loan_perf_data AS
 SELECT LOAN_ID, 
 	to_date(MONTHLY_RPT_PRD) as MONTHLY_RPT_PRD, 
-	LAST_RT,
-	LAST_UPB, 
-	LOAN_AGE, 
-	MONS_TO_LEGAL_MAT,
-	DELQ_STATUS, 
+	cast(LAST_RT as decimal(4,3)) as LAST_RT,
+	cast(LAST_UPB as bigint) as LAST_UPB, 
+	cast(LOAN_AGE as smallint) as LOAN_AGE, 
+	cast(MONS_TO_LEGAL_MAT as smallint) as MONS_TO_LEGAL_MAT,
+	cast(DELQ_STATUS as smallint) as DELQ_STATUS, 
 	MOD_FLAG,
 	ZERO_BAL_CODE, 
 	to_date(ZB_DTE) as ZB_DTE, 
 	to_date(LPI_DTE) as LPI_DTE,
-	NS_PROCS, 
+	cast(NS_PROCS as bigint) as NS_PROCS, 
 	"FANNIE_MAE" as AGENCY
 from fannie_mae_perf
 UNION ALL
 SELECT 
-	fred_p.ID_loan as LOAN_ID, 
+	ID_loan as LOAN_ID, 
 	to_date(Period) as MONTHLY_RPT_PRD, 
-	New_Int_rt as LAST_RT,
-	Act_endg_upb as LAST_UPB, 
-	loan_age as LOAN_AGE, 
-	mths_remng as MONS_TO_LEGAL_MAT,
-	delq_sts as DELQ_STATUS, 
+	cast(New_Int_rt as decimal(4,3)) as LAST_RT,
+	cast(Act_endg_upb as bigint) as LAST_UPB, 
+	cast(loan_age as smallint) as LOAN_AGE, 
+	cast(mths_remng as smallint) as MONS_TO_LEGAL_MAT,
+	cast(delq_sts as smallint) as DELQ_STATUS, 
 	flag_mod as MOD_FLAG,
 	CD_Zero_BAL as ZERO_BAL_CODE, 
 	to_date(Dt_zero_BAL) as ZB_DTE, 
 	to_date(Dt_Lst_Pi) as LPI_DTE,
-	Net_Sale_Proceeds as NS_PROCS, 
+	cast(Net_Sale_Proceeds as bigint) as NS_PROCS, 
 	"FREDDIE_MAC" as AGENCY
-from freddie_mac_perf fred_p
+from freddie_mac_perf
