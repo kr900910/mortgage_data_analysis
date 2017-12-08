774b1c61fef0de3ee4305ed3741f6ea2ba69f891
Alyssa <alyssa.eisenberg@gmail.com>
2017-12-07 23:19:04 -0500

    Adjusted to_date type formatting

--- transforming/acq_perf_data.sql
+++ transforming/acq_perf_data.sql
@@ -1,7 +1,8 @@
 DROP TABLE acq_perf_data;
 CREATE TABLE acq_perf_data AS
 SELECT acq.*, 
-	perf.MONTHLY_RPT_PRD,
+	perf.RPT_PRD_MTH,
+	perf.RPT_PRD_YR,
 	perf.LAST_RT,
 	perf.LAST_UPB,
 	perf.LOAN_AGE, 
@@ -9,8 +10,10 @@ SELECT acq.*,
 	perf.DELQ_STATUS, 
 	perf.MOD_FLAG,
 	perf.ZERO_BAL_CODE, 
-	perf.ZB_DTE, 
-	perf.LPI_DTE,
+	perf.ZB_DTE_MTH,
+	perf.ZB_DTE_YR, 
+	perf.LPI_DTE_MTH,
+	perf.LPI_DTE_YR,
 	perf.NS_PROCS, 
 	perf.AGENCY as PERF_AGENCY
 from loan_acq_data acq
--- transforming/loan_perf_data.sql
+++ transforming/loan_perf_data.sql
@@ -1,7 +1,8 @@
 DROP TABLE loan_perf_data;
 CREATE TABLE loan_perf_data AS
 SELECT LOAN_ID, 
-	to_date(MONTHLY_RPT_PRD) as MONTHLY_RPT_PRD, 
+	cast(substr(MONTHLY_RPT_PRD,1,2) as smallint) as RPT_PRD_MTH,
+	cast(substr(MONTHLY_RPT_PRD,7) as smallint) as RPT_PRD_YR, 
 	cast(LAST_RT as decimal(4,3)) as LAST_RT,
 	cast(LAST_UPB as bigint) as LAST_UPB, 
 	cast(LOAN_AGE as smallint) as LOAN_AGE, 
@@ -9,15 +10,18 @@ SELECT LOAN_ID,
 	cast(DELQ_STATUS as smallint) as DELQ_STATUS, 
 	MOD_FLAG,
 	ZERO_BAL_CODE, 
-	to_date(ZB_DTE) as ZB_DTE, 
-	to_date(LPI_DTE) as LPI_DTE,
+	cast(substr(ZB_DTE, 1, 2)) AS ZB_DTE_MTH,
+	cast(substr(ZB_DTE, 4)) AS ZB_DTE_YR,
+	cast(substr(LPI_DTE,1,2) as smallint) as LPI_DTE_MTH,
+	cast(substr(LPI_DTE,7) as smallint) as LPI_DTE_YR, 
 	cast(NS_PROCS as bigint) as NS_PROCS, 
 	"FANNIE_MAE" as AGENCY
 from fannie_mae_perf
 UNION ALL
 SELECT 
 	ID_loan as LOAN_ID, 
-	to_date(Period) as MONTHLY_RPT_PRD, 
+	cast(substr(Period,5) as smallint) as RPT_PRD_MTH, 
+	cast(substr(Period,1,4) as smallint) as RPT_PRD_YR, 
 	cast(New_Int_rt as decimal(4,3)) as LAST_RT,
 	cast(Act_endg_upb as bigint) as LAST_UPB, 
 	cast(loan_age as smallint) as LOAN_AGE, 
@@ -25,8 +29,10 @@ SELECT
 	cast(delq_sts as smallint) as DELQ_STATUS, 
 	flag_mod as MOD_FLAG,
 	CD_Zero_BAL as ZERO_BAL_CODE, 
-	to_date(Dt_zero_BAL) as ZB_DTE, 
-	to_date(Dt_Lst_Pi) as LPI_DTE,
+	cast(substr(Dt_zero_BAL,5) as smallint) as ZB_DTE_MTH, 
+	cast(substr(Dt_zero_BAL,1,4) as smallint) as ZB_DTE_YR, 
+	cast(substr(Dt_Lst_Pi,5) as smallint) as LPI_DTE_MTH, 
+	cast(substr(Dt_Lst_Pi,1,4) as smallint) as LPI_DTE_YR, 
 	cast(Net_Sale_Proceeds as bigint) as NS_PROCS, 
 	"FREDDIE_MAC" as AGENCY
 from freddie_mac_perf
